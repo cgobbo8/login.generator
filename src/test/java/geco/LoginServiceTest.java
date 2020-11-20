84f32b35edc44a077c749e59a0a3160032e8e10e
cgobbo8 <corentin.gobbo@hotmail.fr>
2020-11-20 08:52:52 +0100

    Ajout fichier LoginServiceTest et remove AppTest

---
+++ src/test/java/geco/LoginServiceTest.java
@@ -0,0 +1,29 @@
+package geco;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LoginServiceTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void loginExists() {
+    }
+
+    @Test
+    public void addLogin() {
+    }
+
+    @Test
+    public void findAllLoginsStartingWith() {
+    }
+
+    @Test
+    public void findAllLogins() {
+    }
+}
